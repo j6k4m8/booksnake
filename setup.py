a7e38ada5c8896351974b7c566e0cca15b05cff0
Jordan Matelsky <j6k4m8@gmail.com>
2016-11-25 11:24:05 -0500

    Prepare for PyPI registration

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
-from setuptools import setup
+from distutils.core import setup
+
+VERSION = "0.0.1"
 
 # https://pythonhosted.org/an_example_pypi_project/setuptools.html
 def read(fname):
@@ -7,21 +9,21 @@ def read(fname):
 
 setup(
     name = "booksnake",
-    version = "0.0.1",
+    version = VERSION,
     author = "Jordan Matelsky",
     author_email = "j6k4m8@gmail.com",
     description = ("Download books from the internet and send to kindle"),
     license = "BSD",
-    keywords = "kindle ebook download",
-    url = "https://github.com/j6k4m8/booksnake",
+    keywords = [
+        "kindle",
+        "ebook",
+        "download"
+    ],
+    url = "https://github.com/j6k4m8/booksnake/tarball/" + VERSION,
     packages=['booksnake'],
     long_description = read('README.md'),
     scripts = [
         'scripts/booksnake'
-    ]
-    # classifiers=[
-    #     "Development Status :: 3 - Alpha",
-    #     "Topic :: Utilities",
-    #     "License :: OSI Approved :: BSD License",
-    # ],
+    ],
+    classifiers=[]
 )
