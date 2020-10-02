c21ce1e3d239bb68c16562d16f8a174eb592dc03
Jordan Matelsky <j6k4m8@gmail.com>
2020-10-02 14:01:25 -0400

    v0.3.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+from os import path
 from setuptools import setup, find_packages
 
 import booksnake
@@ -6,19 +6,27 @@ from booksnake.version import __version__
 
 VERSION = __version__
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="booksnake",
     version=VERSION,
     author="Jordan Matelsky",
     author_email="j6k4m8@gmail.com",
     description=("Download books from the internet and send to kindle"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="Apache 2.0",
     keywords=[
         "kindle",
         "ebook",
         "download"
     ],
-    url="https://github.com/j6k4m8/booksnake/tarball/" + VERSION,
+    url="https://github.com/j6k4m8/booksnake/",
     packages=find_packages(exclude=["docs", "tests*"]),
     scripts=[
         'scripts/booksnake'
