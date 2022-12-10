5e798192a1238a8b220a619ee8db3b9722e083bc
Jordan Matelsky <j6k4m8@gmail.com>
2022-12-10 16:50:45 -0500

    Remove broken searcher

--- booksnake/__init__.py
+++ booksnake/__init__.py
@@ -1,4 +1,5 @@
 import urllib
+
 #!/usr/bin/env python3
 
 """
@@ -17,20 +18,19 @@ import sys
 
 import requests
 from libgen_api import LibgenSearch
-import pandas as pd
+
+# import pandas as pd
 import bs4 as bs
 
 from .sending import send_file
 
-SUPPORTED_EXTENSIONS = ["mobi", "azw", "text", "txt"]
+SUPPORTED_EXTENSIONS = ["mobi", "azw", "text", "txt", "epub", "rtf"]
 SUPPORTED_LANGUAGES = ["english"]
 
 
 class Book:
-
     def __init__(self, title: str, author: str, ext: str) -> None:
-        """
-        """
+        """ """
         self.title = title
         self.author = author
         self.ext = ext
@@ -47,18 +47,17 @@ class Book:
 
 
 class Searcher:
-
     def __init__(self) -> None:
-        """
-        """
+        """ """
 
     def search(self, query: str) -> List[Book]:
         ...
 
 
 class LibgenBook(Book):
-
-    def __init__(self, title: str, author: str, ext: str, mirrors: List[str], data: dict = None) -> None:
+    def __init__(
+        self, title: str, author: str, ext: str, mirrors: List[str], data: dict = None
+    ) -> None:
         self.title = title
         self.author = author
         self.ext = ext
@@ -71,18 +70,19 @@ class LibgenBook(Book):
             title=data.get("Title", data.get("title")),
             author=data.get("Author", data.get("author")),
             ext=data.get("Extension", data.get("ext")),
-            mirrors=data.get("links", None) or [v for k, v in data.items() if "Mirror" in k],
-            data=data
+            mirrors=data.get("links", None)
+            or [v for k, v in data.items() if "Mirror" in k],
+            data=data,
         )
 
     def to_dict(self) -> dict:
         return {
-            'title': self.title,
-            'author': self.author,
-            'ext': self.ext,
-            'links': self.mirrors,
-            'data': self._data,
-            "type": "LibgenBook"
+            "title": self.title,
+            "author": self.author,
+            "ext": self.ext,
+            "links": self.mirrors,
+            "data": self._data,
+            "type": "LibgenBook",
         }
 
     def __repr__(self) -> str:
@@ -95,11 +95,11 @@ class LibgenBook(Book):
         for mirror in self.mirrors:
             try:
                 content = requests.get(mirror).content
-                soup = bs.BeautifulSoup(content, 'lxml')
-                download_link = soup.find_all("a", text="GET")[0]['href']
+                soup = bs.BeautifulSoup(content, "lxml")
+                download_link = soup.find_all("a", text="GET")[0]["href"]
                 book_contents = requests.get(download_link).content
                 if destination:
-                    with open(destination, 'wb') as fh:
+                    with open(destination, "wb") as fh:
                         fh.write(book_contents)
                     return
                 else:
@@ -110,7 +110,6 @@ class LibgenBook(Book):
 
 
 class LibgenSearcher(Searcher):
-
     def __init__(self) -> None:
         super().__init__()
 
@@ -123,59 +122,59 @@ class LibgenSearcher(Searcher):
             LibgenBook.from_dict(r)
             for r in res
             if (
-                r['Extension'].lower() in SUPPORTED_EXTENSIONS and
-                r['Language'].lower() in SUPPORTED_LANGUAGES
+                r["Extension"].lower() in SUPPORTED_EXTENSIONS
+                and r["Language"].lower() in SUPPORTED_LANGUAGES
             )
         ]
 
 
-class LibgenFictionSearcher(Searcher):
+# class LibgenFictionSearcher(Searcher):
+#     def __init__(self, base_url: str = "http://libgen.is/fiction/") -> None:
+#         self.base_url = base_url
 
-    def __init__(self, base_url: str = "http://libgen.rs/fiction/") -> None:
-        self.base_url = base_url
+#     def search(self, query: str) -> List[Book]:
 
-    def search(self, query: str) -> List[Book]:
+#         f = requests.get(f"{self.base_url}?q={query}").content
+#         soup = bs.BeautifulSoup(f, "lxml")
+#         parsed_table = soup.find_all("table")[0]
+#         data = [
+#             [
+#                 # {tag.text.split(" ")[0]: tag["href"] for tag in td.find_all("a")}
+#                 # if td.find_all("a")
+#                 # else
+#                 "".join(td.stripped_strings).replace("&varr;", "")
+#                 for td in row.find_all("td")
+#             ]
+#             for row in parsed_table.find_all("tr")
+#         ]
+#         table = pd.DataFrame(data[1:], columns=data[0])
 
-        f = requests.get(f"{self.base_url}?q={query}").content
-        soup = bs.BeautifulSoup(f, 'lxml')
-        parsed_table = soup.find_all('table')[0]
-        data = [
-            [
-                {
-                    tag.text: tag['href']
-                    for tag in td.find_all("a")
-                } if td.find_all('a') else ''.join(td.stripped_strings)
-                for td in row.find_all('td')
-            ]
-            for row in parsed_table.find_all('tr')
-        ]
-        table = pd.DataFrame(data[1:], columns=data[0])
 
-        unfiltered_results = [
-            LibgenBook(
-                title="".join(row.Title.keys()),
-                author=(
-                    row["Author(s)"] if isinstance(row["Author(s)"], str)
-                    else "".join(row["Author(s)"].keys())
-                ),
-                ext=row["File"].split("/")[0].lower().strip(),
-                mirrors=list(row["Mirrors"].values()),
-                data=dict(row)
-            )
-            for _, row in table.iterrows()
-        ]
-        return [
-            book
-            for book in unfiltered_results
-            if (
-                book.ext in SUPPORTED_EXTENSIONS and
-                book._data['Language'].lower() in SUPPORTED_LANGUAGES
-            )
-        ]
+#         unfiltered_results = [
+#             LibgenBook(
+#                 title="".join(row.Title.keys()),
+#                 author=(
+#                     row["Author(s)"]
+#                     if isinstance(row["Author(s)"], str)
+#                     else "".join(row["Author(s)"].keys())
+#                 ),
+#                 ext=row["File"].split("/")[0].lower().strip(),
+#                 mirrors=list(row["Mirrors"].values()),
+#                 data=dict(row),
+#             )
+#             for _, row in table.iterrows()
+#         ]
+#         return [
+#             book
+#             for book in unfiltered_results
+#             if (
+#                 book.ext in SUPPORTED_EXTENSIONS
+#                 and book._data["Language"].lower() in SUPPORTED_LANGUAGES
+#             )
+#         ]
 
 
 class GutenbergBook(Book):
-
     def __init__(self, author, title, ext, link, data: dict = None) -> None:
         self.author = author
         self.title = title
@@ -196,23 +195,23 @@ class GutenbergBook(Book):
             "ext": self.ext,
             "links": [self.link],
             "data": self._data,
-            "type": "GutenbergBook"
+            "type": "GutenbergBook",
         }
 
     @staticmethod
     def from_dict(data: dict) -> Book:
         return GutenbergBook(
-            author=data['author'],
-            title=data['title'],
-            ext=data['ext'],
-            link=data['link'],
-            data=data['data'],
+            author=data["author"],
+            title=data["title"],
+            ext=data["ext"],
+            link=data["link"],
+            data=data["data"],
         )
 
     def download(self, destination: str = None) -> Union[str, io.BytesIO]:
         book_content = requests.get(self.link).content
         if destination:
-            with open(destination, 'wb') as fh:
+            with open(destination, "wb") as fh:
                 fh.write(book_content)
             return
         else:
@@ -228,56 +227,51 @@ class GutenbergSearcher(Searcher):
         self.base_url = "https://www.gutenberg.org"
 
     def search(self, query):
-        query = query.replace(' ', '+')
+        query = query.replace(" ", "+")
         query = "/ebooks/search/?query=" + query
         f = urllib.request.FancyURLopener({}).open(self.base_url + query)
         try:
             content = f.read()
         except Exception:
             Warning(
-                "You've probably hit the Gutenberg page too many times " +
-                "and have been banned for 24 hours by their rate-limiter. "
+                "You've probably hit the Gutenberg page too many times "
+                + "and have been banned for 24 hours by their rate-limiter. "
             )
             return []
-        soup = bs.BeautifulSoup(content, 'html.parser')
+        soup = bs.BeautifulSoup(content, "html.parser")
         links = soup.findAll("li", {"class": "booklink"})
         options = []
         for a in links:
-            author = a.find('span', {
-                'class': 'subtitle'
-            })
+            author = a.find("span", {"class": "subtitle"})
             author = author.text.strip() if author else " "
-            title = a.find('span', {
-                'class': 'title'
-            }).text.strip()
+            title = a.find("span", {"class": "title"}).text.strip()
 
             options.append(
                 GutenbergBook(
                     author=author,
                     title=title,
                     ext="mobi",
-                    link=self.base_url + (
-                        a.find('a').get('href') +
-                        ".kindle.images"
-                    ),
+                    link=self.base_url + (a.find("a").get("href") + ".kindle.images"),
                 )
             )
         return options
 
 
 DEFAULT_SEARCHERS = [
-    # (GutenbergSearcher, ()),
-    # (LibgenSearcher, ()),
-    (LibgenFictionSearcher, ()),
+    (GutenbergSearcher, ()),
+    (LibgenSearcher, ()),
+    # (LibgenFictionSearcher, ()),
 ]
 
 
 def cli():
-
     def _cli_list(query) -> List[Book]:
         results = []
         for searcher, args in DEFAULT_SEARCHERS:
-            results = [*results, *searcher(*args).search(query)]
+            try:
+                results = [*results, *searcher(*args).search(query)]
+            except Exception as e:
+                print(f"Error in {searcher}: {e}")
 
         for i, result in enumerate(results):
             print(f"{i+1}:\t{result}")
@@ -310,12 +304,11 @@ def cli():
             contents=contents,
             settings=json.load(
                 open(os.path.expanduser("~/.config/booksnake.json"), "r")
-            )
+            ),
         )
 
     def _cli_fail(query):
-        print(
-            "Something went wrong. Try booksnake [download|search|send] QUERY")
+        print("Something went wrong. Try booksnake [download|search|send] QUERY")
 
     commands = {
         "download": _cli_download,
