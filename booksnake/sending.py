f356edf9655c8313ed2206aff1fdf27c590b1049
Jordan Matelsky <j6k4m8@gmail.com>
2024-06-08 13:32:33 -0400

    Add argparser for cli handler, and abstract the config file path

--- booksnake/sending.py
+++ booksnake/sending.py
@@ -1,12 +1,20 @@
 import smtplib
-import mimetypes
 import email
 import email.mime.application
+
 try:
     import email.mime.Multipart
 except Exception as exc:
     import email.mime.multipart
 import getpass
+
+BLANK_SETTINGS = {
+    "from_email": None,
+    "to_email": None,
+    "smtp_password": None,
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587,
+}
 
 
 def send_file(filename, contents=None, from_email=None, to_email=None, settings=None):
@@ -14,62 +22,57 @@ def send_file(filename, contents=None, from_email=None, to_email=None, settings=
     cleanups = []
     # http://stackoverflow.com/a/8243031/979255
     if from_email is None:
-        if 'from_email' not in settings:
+        if "from_email" not in settings:
             raise ValueError("No from_email supplied.")
-        from_email = settings['from_email']
+        from_email = settings["from_email"]
 
     if to_email is None:
-        if 'to_email' not in settings:
+        if "to_email" not in settings:
             raise ValueError("No to_email supplied.")
-        to_email = settings['to_email']
+        to_email = settings["to_email"]
 
     try:
         m = email.mime.Multipart.MIMEMultipart()
     except Exception as exc:
         m = email.mime.multipart.MIMEMultipart()
-    m['Subject'] = ''
-    m['From'] = from_email
-    m['To'] = to_email
+    m["Subject"] = ""
+    m["From"] = from_email
+    m["To"] = to_email
 
     if contents:
         att = email.mime.application.MIMEApplication(
-            contents.read(),
-            _subtype="x-mobipocket-ebook"
+            contents.read(), _subtype="x-mobipocket-ebook"
         )
     elif isinstance(filename, str):
-        fp = open(filename, 'rb')
+        fp = open(filename, "rb")
         att = email.mime.application.MIMEApplication(
-            fp.read(),
-            _subtype="x-mobipocket-ebook"
+            fp.read(), _subtype="x-mobipocket-ebook"
         )
         fp.close()
     else:
         raise ValueError("Must specify filename, or filename and contents")
-    att.add_header('Content-Disposition', 'attachment', filename=filename)
+    att.add_header("Content-Disposition", "attachment", filename=filename)
     m.attach(att)
 
     cleanups.append(filename)
 
     while True:
         try:
-            if (
-                ('smtp_password' not in settings) or
-                (settings['smtp_password'] in [None, ""])
+            if ("smtp_password" not in settings) or (
+                settings["smtp_password"] in [None, ""]
             ):
-                passwd = getpass.getpass(
-                    "SMTP password for {}: ".format(from_email)
-                )
+                passwd = getpass.getpass("SMTP password for {}: ".format(from_email))
             else:
-                passwd = settings['smtp_password']
+                passwd = settings["smtp_password"]
 
             # pretty_print([CYAN], "Beginning send...")
-            s = smtplib.SMTP('smtp.gmail.com:587')
+            s = smtplib.SMTP(
+                settings.get("smtp_server", "smtp.gmail.com"),
+                settings.get("smtp_port", 587),
+            )
             s.starttls()
             s.login(from_email, passwd)
-            s.sendmail(
-                from_email,
-                [to_email],
-                m.as_string())
+            s.sendmail(from_email, [to_email], m.as_string())
             s.quit()
             # pretty_print([CYAN], "Send complete.")
             return cleanups
--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from os import path
 from setuptools import setup, find_packages
 
-import booksnake
-from booksnake.version import __version__
-
-VERSION = __version__
+VERSION = "0.3.1"
 
 here = path.abspath(path.dirname(__file__))
 
@@ -16,27 +13,23 @@ setup(
     name="booksnake",
     version=VERSION,
     author="Jordan Matelsky",
-    author_email="j6k4m8@gmail.com",
+    author_email="opensource@matelsky.com",
     description=("Download books from the internet and send to kindle"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="Apache 2.0",
-    keywords=[
-        "kindle",
-        "ebook",
-        "download"
-    ],
+    keywords=["kindle", "ebook", "download"],
     url="https://github.com/j6k4m8/booksnake/",
     packages=find_packages(exclude=["docs", "tests*"]),
-    scripts=[
-        'scripts/booksnake'
-    ],
+    scripts=["scripts/booksnake"],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'beautifulsoup4',
-        'libgen-api'
-    ],
+    install_requires=["beautifulsoup4", "libgen-api", "requests"],
 )
